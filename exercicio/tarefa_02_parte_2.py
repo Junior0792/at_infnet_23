55bcd44565c39e8928d7f1bf0e072b23294609fe
Junior Roso <83788547+Junior0792@users.noreply.github.com>
2023-12-19 15:51:42 -0300

    Update tarefa_02_parte_2.py

--- exercicio/tarefa_02_parte_2.py
+++ exercicio/tarefa_02_parte_2.py
@@ -1,21 +1,21 @@
-def troco_programacao_dinamica(valor, moedas):
-    # Inicializa uma lista para armazenar o número mínimo de moedas para cada valor de troco
-    qtd_moedas = [float('inf')] * (valor + 1)
-    qtd_moedas[0] = 0  # Para o troco zero, não são necessárias moedas
-    
-    # Itera sobre todos os valores de troco de 1 até o valor desejado
-    for i in range(1, valor + 1):
-        # Para cada valor de troco, percorre todas as moedas disponíveis
-        for moeda in moedas:
+
+def calcular_menor_numero_moedas_programacao_dinamica(valor_troco, tipos_de_moedas):
+    # Cria uma tabela para armazenar o número mínimo de moedas para cada valor de troco
+    tabela_dp = [float('inf')] * (valor_troco + 1)
+    tabela_dp[0] = 0  # O troco de 0 requer 0 moedas
+
+    # Percorre todos os valores de troco de 1 até o valor desejado
+    for i in range(1, valor_troco + 1):
+        # Para cada valor de troco, verifica todas as moedas disponíveis
+        for moeda in tipos_de_moedas:
             if moeda <= i:
-                # Atualiza o número mínimo de moedas necessário para o valor de troco atual
-                qtd_moedas[i] = min(qtd_moedas[i], 1 + qtd_moedas[i - moeda])
-    
-    return qtd_moedas[valor]
+                # Atualiza a tabela com o menor número de moedas necessário para o valor de troco atual
+                tabela_dp[i] = min(tabela_dp[i], tabela_dp[i - moeda] + 1)
 
-# Input dos valores
-troco = int(input("Digite o valor do troco: "))
-moedas_algorithia = list(map(int, input("Digite os valores das moedas separados por espaço: ").split()))
+    return tabela_dp[valor_troco]  # Retorna o menor número de moedas necessário para o valor de troco desejado
 
-# Chama a função para encontrar o número mínimo de moedas usando programação dinâmica
-print("Número mínimo de moedas usando programação dinâmica:", troco_programacao_dinamica(troco, moedas_algorithia))
+moedas_algoritmos = [1, 5, 10, 21, 25]
+valor_compra = 37
+qtd_moedas = calcular_menor_numero_moedas_programacao_dinamica(valor_compra, moedas_algoritmos)
+
+print(f"Número mínimo de moedas: {qtd_moedas}")
