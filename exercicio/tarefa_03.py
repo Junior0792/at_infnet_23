443c80ab0306ccaf787c6ec37aee67a919635d64
Junior Roso <83788547+Junior0792@users.noreply.github.com>
2023-12-19 16:43:18 -0300

    Update tarefa_03.py

--- exercicio/tarefa_03.py
+++ exercicio/tarefa_03.py
@@ -13,62 +13,41 @@ import heapq
 # Digite origem, destino e peso da aresta 3 (separados por espaço): B C 5
 # Digite origem, destino e peso da aresta 4 (separados por espaço): B D 10
 # Digite origem, destino e peso da aresta 5 (separados por espaço): C D 3
-# Digite o vértice de origem para calcular as distâncias: A
+import heapq
 
+def dijkstra(graph, start):
+    # Inicialização das estruturas de dados
+    distances = {vertex: float('inf') for vertex in graph}
+    distances[start] = 0
+    priority_queue = [(0, start)]
+    
+    while priority_queue:
+        current_distance, current_vertex = heapq.heappop(priority_queue)
+        
+        # Se a distância atual for maior que a armazenada, ignoramos
+        if current_distance > distances[current_vertex]:
+            continue
+        
+        for neighbor, weight in graph[current_vertex].items():
+            distance = current_distance + weight
+            
+            # Se encontrarmos um caminho mais curto, atualizamos
+            if distance < distances[neighbor]:
+                distances[neighbor] = distance
+                heapq.heappush(priority_queue, (distance, neighbor))
+    
+    return distances
 
+# Exemplo de utilização:
+# Representação do grafo como dicionário de adjacência
+graph = {
+    'A': {'B': 3, 'C': 2},
+    'B': {'A': 3, 'C': 1, 'D': 5},
+    'C': {'A': 2, 'B': 1, 'D': 4},
+    'D': {'B': 5, 'C': 4}
+}
 
+start_vertex = 'A'
+result = dijkstra(graph, start_vertex)
+print("Distâncias mínimas a partir do vértice inicial {}: {}".format(start_vertex, result))
 
-class Grafo:
-    def __init__(self):
-        self.vertices = {}
-
-    def adicionar_vertice(self, vertice):
-        self.vertices[vertice] = {}
-
-    def adicionar_aresta(self, origem, destino, peso):
-        self.vertices[origem][destino] = peso
-        # Para um grafo não direcionado, descomente a linha abaixo
-        # self.vertices[destino][origem] = peso
-
-    def dijkstra(self, origem):
-        # Inicializa as distâncias com infinito para todos os vértices, exceto a origem
-        distancias = {vertice: float('inf') for vertice in self.vertices}
-        distancias[origem] = 0
-
-        # Inicializa a fila de prioridade com a origem e sua distância zero
-        fila = [(0, origem)]
-
-        while fila:
-            # Obtém o vértice com menor distância
-            distancia_atual, vertice_atual = heapq.heappop(fila)
-
-            # Para cada vizinho do vértice atual
-            for vizinho, peso in self.vertices[vertice_atual].items():
-                # Calcula a nova distância até o vizinho
-                nova_distancia = distancia_atual + peso
-
-                # Se a nova distância for menor que a distância armazenada
-                if nova_distancia < distancias[vizinho]:
-                    # Atualiza a distância e insere na fila de prioridade
-                    distancias[vizinho] = nova_distancia
-                    heapq.heappush(fila, (nova_distancia, vizinho))
-
-        return distancias
-
-# Input dos dados do grafo
-grafo = Grafo()
-
-num_vertices = int(input("Digite o número de vértices: "))
-for i in range(num_vertices):
-    vertice = input(f"Digite o nome do vértice {i + 1}: ")
-    grafo.adicionar_vertice(vertice)
-
-num_arestas = int(input("Digite o número de arestas: "))
-for i in range(num_arestas):
-    origem, destino, peso = input(f"Digite origem, destino e peso da aresta {i + 1} (separados por espaço): ").split()
-    peso = int(peso)
-    grafo.adicionar_aresta(origem, destino, peso)
-
-origem = input("Digite o vértice de origem para calcular as distâncias: ")
-distancias = grafo.dijkstra(origem)
-print(f"Distâncias a partir do vértice {origem}: {distancias}")
