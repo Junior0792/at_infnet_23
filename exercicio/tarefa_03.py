6f18e462bc45e43042a1a64161a1d5da59ee19c3
Junior Roso <83788547+Junior0792@users.noreply.github.com>
2023-12-20 12:22:35 -0300

    Update tarefa_03.py

--- exercicio/tarefa_03.py
+++ exercicio/tarefa_03.py
@@ -1,38 +1,57 @@
 import heapq
 
-def dijkstra(graph, start):
-    # Inicialização das estruturas de dados
-    distances = {vertex: float('inf') for vertex in graph}
-    distances[start] = 0
-    priority_queue = [(0, start)]
-    
-    while priority_queue:
-        current_distance, current_vertex = heapq.heappop(priority_queue)
+class Grafo:
+    def __init__(self):
+        self.vertices = {}
+
+    def adicionar_vertice(self, vertice):
+        self.vertices[vertice] = {}
+
+    def adicionar_aresta(self, origem, destino, peso):
+        self.vertices[origem][destino] = peso
+        self.vertices[destino][origem] = peso
+
+    def calcular_menor_caminho(self, vertice_origem):
+        # Inicializa as distâncias com infinito para todos os vértices
+        distancias = {v: float('inf') for v in self.vertices}
+        # Define a distância do vértice origem para ele mesmo como 0
+        distancias[vertice_origem] = 0
         
-        # Se a distância atual for maior que a armazenada, ignoramos
-        if current_distance > distances[current_vertex]:
-            continue
-        
-        for neighbor, weight in graph[current_vertex].items():
-            distance = current_distance + weight
-            
-            # Se encontrarmos um caminho mais curto, atualizamos
-            if distance < distances[neighbor]:
-                distances[neighbor] = distance
-                heapq.heappush(priority_queue, (distance, neighbor))
-    
-    return distances
+        # Fila de prioridade para armazenar os vértices a serem explorados
+        fila_prioridade = [(0, vertice_origem)]
 
-# Exemplo de utilização:
-# Representação do grafo como dicionário de adjacência
-graph = {
-    'A': {'B': 3, 'C': 2},
-    'B': {'A': 3, 'C': 1, 'D': 5},
-    'C': {'A': 2, 'B': 1, 'D': 4},
-    'D': {'B': 5, 'C': 4}
-}
+        while fila_prioridade:
+            # Obtém o vértice com a menor distância atual da fila
+            dist_atual, vertice_atual = heapq.heappop(fila_prioridade)
 
-start_vertex = 'A'
-result = dijkstra(graph, start_vertex)
-print("Distâncias mínimas a partir do vértice inicial {}: {}".format(start_vertex, result))
+            # Se a distância atual for maior que a distância registrada, ignora
+            if dist_atual > distancias[vertice_atual]:
+                continue
 
+            # Itera sobre os vizinhos do vértice atual
+            for vizinho, peso in self.vertices[vertice_atual].items():
+                # Calcula a distância através do vértice atual até o vizinho
+                dist = dist_atual + peso
+                # Se a distância calculada for menor que a registrada, atualiza
+                if dist < distancias[vizinho]:
+                    distancias[vizinho] = dist
+                    # Insere o vizinho na fila de prioridade com sua nova distância
+                    heapq.heappush(fila_prioridade, (dist, vizinho))
+
+        return distancias
+
+# Exemplo de uso:
+grafo = Grafo()
+grafo.adicionar_vertice('A')
+grafo.adicionar_vertice('B')
+grafo.adicionar_vertice('C')
+grafo.adicionar_vertice('D')
+
+grafo.adicionar_aresta('A', 'B', 4)
+grafo.adicionar_aresta('A', 'C', 2)
+grafo.adicionar_aresta('B', 'C', 5)
+grafo.adicionar_aresta('B', 'D', 10)
+grafo.adicionar_aresta('C', 'D', 3)
+
+distancias = grafo.calcular_menor_caminho('A')
+print("Distâncias a partir de A:", distancias)
