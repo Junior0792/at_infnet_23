437879a520eeab0342131583fea367e600513586
Junior Roso <83788547+Junior0792@users.noreply.github.com>
2023-12-20 13:33:54 -0300

    Update tarefa_01.py

--- exercicio/tarefa_01.py
+++ exercicio/tarefa_01.py
@@ -1,63 +1,72 @@
 import random
 
-# Função para criar a população inicial
-def criar_pop(tam_pop, tam_genes):
-    return [[random.randint(0, 1) for _ in range(tam_genes)] for _ in range(tam_pop)]
+# Função para gerar um indivíduo aleatório
+def gerar_individuo(tamanho):
+    return [random.randint(0 , 1) for _ in range(tamanho)]
+
+# Função para gerar a população inicial
+def gerar_populacao_inicial(tamanho_populacao, tamanho_individuo):
+    return [gerar_individuo(tamanho_individuo) for _ in range(tamanho_populacao)]
 
 # Função para calcular a aptidão de um indivíduo
-def calc_apt(individuo):
+def calcular_aptidao(individuo):
     return sum(individuo)
 
-# Função para ordenar a população com base na aptidão
-def ordenar(populacao):
-    return sorted(populacao, key=calc_apt, reverse=True)
-
 # Função para realizar o crossover entre dois indivíduos
-def crossover(ind1, ind2):
-    ponto_cross = random.randint(1, len(ind1) - 1)
-    novo_ind1 = ind1[:ponto_cross] + ind2[ponto_cross:]
-    novo_ind2 = ind2[:ponto_cross] + ind1[ponto_cross:]
-    return novo_ind1, novo_ind2
+def crossover(individuo1, individuo2):
+    ponto_crossover = random.randint(1, len(individuo1) - 1)
+    filho1 = individuo1[:ponto_crossover] + individuo2[ponto_crossover:]
+    filho2 = individuo2[:ponto_crossover] + individuo1[ponto_crossover:]
+    return filho1, filho2
 
 # Função para realizar a mutação em um indivíduo
-def mutacao(individuo, taxa_mut):
-    for i in range(len(individuo)):
-        if random.random() < taxa_mut:
-            num_genes_mut = random.randint(1, 3)
-            genes_mut = random.sample(range(len(individuo)), num_genes_mut)
-            for gene in genes_mut:
-                individuo[gene] = 1 if individuo[gene] == 0 else 0
-    return individuo
+def mutacao(individuo):
+    qtd_genes_mutacao = random.randint(1, 3)
+    for _ in range(qtd_genes_mutacao):
+        indice = random.randint(0, len(individuo) - 1)
+        individuo[indice] = 1 if individuo[indice] == 0 else 0
 
-# Solicitar parâmetros do usuário
-tam_populacao = int(input("Tamanho da população inicial: "))
-tam_genes = int(input("Tamanho dos genes dos indivíduos: "))
-max_geracoes = int(input("Número máximo de gerações: "))
-taxa_mutacao = float(input("Taxa de mutação (entre 0 e 1): "))
+# Função para ordenar a população com base na aptidão
+def ordenar_populacao(populacao):
+    return sorted(populacao, key=calcular_aptidao, reverse=True)
 
-# Gerando população inicial
-pop = criar_pop(tam_populacao, tam_genes)
+# Parâmetros do algoritmo genético
+tamanho_populacao = 20
+tamanho_individuo = 6
+max_geracoes = 1000
 
-# Execução do algoritmo genético
+# Gerar população inicial
+populacao = gerar_populacao_inicial(tamanho_populacao, tamanho_individuo)
+
 for geracao in range(max_geracoes):
-    pop = ordenar(pop)
-    if calc_apt(pop[0]) == tam_genes:
-        print("Objetivo alcançado na geração", geracao)
-        break
-    
-    # Seleção dos mais aptos
-    pop = pop[:tam_populacao]
-    
-    # Cruzamento e mutação
-    nova_pop = []
-    while len(nova_pop) < tam_populacao:
-        pai1, pai2 = random.choices(pop, k=2)
-        filho1, filho2 = crossover(pai1, pai2)
-        filho1 = mutacao(filho1, taxa_mutacao)
-        filho2 = mutacao(filho2, taxa_mutacao)
-        nova_pop.extend([filho1, filho2])
-    
-    pop = nova_pop
+    # Calcular aptidão
+    aptidoes = [calcular_aptidao(individuo) for individuo in populacao]
 
-melhor_ind = ordenar(pop)[0]
-print("Melhor indivíduo:", melhor_ind, "Aptidão:", calc_apt(melhor_ind))
+    # Verificar se a função objetivo foi alcançada
+    if max(aptidoes) == tamanho_individuo:
+        print(f"Função objetivo alcançada na geração {geracao + 1}")
+        break
+
+    # Selecionar os indivíduos mais aptos
+    populacao = ordenar_populacao(populacao)[:tamanho_populacao]
+
+    # Realizar crossover
+    nova_populacao = []
+    for _ in range(tamanho_populacao // 2):
+        pai1, pai2 = random.sample(populacao, 2)
+        filho1, filho2 = crossover(pai1, pai2)
+        nova_populacao.extend([filho1, filho2])
+
+    # Aplicar mutação
+    for individuo in nova_populacao[:len(nova_populacao) // 2]:
+        if random.random() < 0.005:  # Probabilidade de mutação de 0,5%
+            mutacao(individuo)
+
+    # Atualizar população
+    populacao = nova_populacao
+
+# Ordenar a população final antes de encerrar
+populacao = ordenar_populacao(populacao)
+print("População final ordenada:")
+for individuo in populacao:
+    print(individuo, "Aptidão:", calcular_aptidao(individuo))
